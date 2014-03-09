c51949bd165d33abd96ba06cc2378fa9a359d1fe
Jouko Karvonen <joukokar@gmail.com>
2014-03-09 00:03:21 +0000

    rails

---
+++ magnet/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magnet::Application.config.secret_key_base = '36f268846f6bb53a973825e61e71dc93f59babc48699dcfc5488a508bf3fb1d108db712d670f452025cafd4650be53174eed922745a70e85ed54e936076a1e09'
